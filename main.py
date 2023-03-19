679d1805d30cc0c58b35a70432009b5d2868b3a4
Romeo-mz <romeo_martinez@etu.u-bourgogne.fr>
2023-03-19 13:04:20 +0100

    V1, need to improve speed

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if os.path.isfile('map.html'):
     os.remove('map.html')
 
 # Coordonnées GPS du centre de la zone à rechercher
-center = (46.0829,-7.4844)
+center = (44.666865, 3.010133)
 
 # Rayon de la zone à rechercher, en kilomètres
 search_radius = 50
@@ -18,26 +18,27 @@ building_radius = 5
 max_buildings = 100
 
 # Calcule la limite supérieure et inférieure de la zone à rechercher
-lat_range = lat_range = (center[0] - 0.009 * search_radius, min(center[0] + 0.009 * search_radius, 90.009))
-lon_range = (-8.4014, -6.5674) #(center[1] - 0.017 * search_radius, center[1] + 0.017 * search_radius)
+lat_range = (center[0] - 0.001 * search_radius, center[0] + 0.001 * search_radius)
+
+lon_range = (center[1] - 0.017 * search_radius, center[1] + 0.017 * search_radius)
 
 
 # Initialise la carte
 map = folium.Map(location=center, zoom_start=10)
 
 # Boucle sur chaque point dans la zone à rechercher
-for lat in [lat_range[0] + x * 0.01 for x in range(int(max(lat_range[0], -90) * 100), int(min(lat_range[1], 90) * 100))]:
-    for lon in [lon_range[0] + x * 0.01 for x in range(int(lon_range[0] * 100), int(lon_range[1] * 100))]:
-        point = (lat, lon)
-
-        print(lat,lon)
+for lat in range (int(lat_range[0] * 10 ), int(lat_range[1] * 10)):
+    for lon in range(int(lon_range[0] * 10), int(lon_range[1] * 10)):
+        point = (lat/10, lon/10)
 
         # Requête l'API OpenStreetMap pour récupérer les informations sur les bâtiments autour du point
         url = f"https://overpass-api.de/api/interpreter?data=[out:json];way(around:{building_radius * 1000},{point[0]},{point[1]})[building];out;"
         response = requests.get(url)
         print(response.content)
+
         if response.content and response.json():
             data = response.json()
+
         else:
             print(f"No data found for point {point}")
             continue
